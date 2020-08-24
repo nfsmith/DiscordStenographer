80373d3c9236a77b1bccbbd2480e21928b95577e
Nick Smith <nickfsmith96@gmail.com>
2020-08-24 17:50:03 -0400

    rewrote main file, improved data handling, still doesnt work half the time, seemingly random

--- DiscordStenographer.js
+++ DiscordStenographer.js
@@ -4,106 +4,115 @@ const path = require('path');
 const opus = require('node-opus');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-//const botCommands = require('./commands');
 const child = require('child_process');
 
-const transcribePCM = 'transcribePCM.py'
-const deletePCM = 'deletePCM.py'
+const transcribePCM = 'transcribePCM.py';
+const deletePCM = 'deletePCM.py';
+const debugLog = 'debug.log';
 
-let voiceConnections = new Map();
-let voiceReceivers = new Map();
-let writeStreams = new Map();
-var saveStream;
-var saveName;
-var echo;
-
-// Object.keys(botCommands).map(key => {
-//   bot.commands.set(botCommands[key].name, botCommands[key]);
-// });
+let voiceConnections = {};
+let logStream = fs.createWriteStream(debugLog);
 
 const TOKEN = process.env.TOKEN;
-
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
+bot.on('message', msg =>{
   if(msg.content.charAt(0) === '!'){
     let req = msg.content.slice(1);
-    textChannel = msg.channel;
+    let textChannel = msg.channel;
+    console.log('Request received: '+req);
     if (req === 'record') {
-      console.log('Recording');
-      start(msg.member);
-      echo = false;
+      start(msg.member, textChannel, false);
     } else if (req === 'off') {
       stop(msg.member);
     } else if (req === 'echo') {
-      console.log("Echoing");
-      start(msg.member);
-      echo = true;
+      start(msg.member, textChannel, true);
+    } else if (req === 'debug') {
+      debug(textChannel);
     }
   }
 });
 
+bot.on('debug', (info) => {
+  logStream.write(info+'\n');
+});
 
-
+let debug = (textChannel) => {
+  textChannel.send({
+    files: [{
+      attachment: debugLog,
+      name: debugLog
+    }]
+  });
+}
 
 bot.on('guildMemberSpeaking', (member, speaking) => {
-  // this event triggers both when someone starts and stops speaking. we only care if they are speaking
   if(speaking.bitfield){
     const ts = new Date(Date.now()).toLocaleString();
-    const rec = voiceReceivers.get(member.voice.channelID);
-    const audio = rec.createStream(member, {mode: 'pcm', end: 'silence' });
+    const conInfo = voiceConnections[member.voice.channelID];
+    const rec = conInfo.receiver;
+    const audio = rec.createStream(member, {mode: 'pcm', end: 'silence'});
     const pcmName = `${Date.now()}.pcm`;
     const outputStream = fs.createWriteStream(pcmName);
     audio.pipe(outputStream);
     audio.on('end', () => {
       let py = child.spawn('python', [transcribePCM, pcmName]);
-      py.stdout.on('data', function (data){
+      py.stdout.on('data', function(data){
         const message = data.toString();
-        if(message[0]!=='!'){ //ignore message if it couldnt be transcribed
+        console.log("transcribePCM returned: "+message);
+        if(message[0]!=='!'){
+          const echo = conInfo.echo;
           if(echo){
-            textChannel.send(member.displayName+": "+message);
-          }else{
-            saveStream.write(ts+" "+member.displayName+": "+message);
+            conInfo.textChannel.send(member.displayName+": "+message);
+          } else {
+            conInfo.saveStream.write(ts+" "+member.displayName+": "+message);
           }
         }
         outputStream.destroy();
         child.spawn('python', [deletePCM]);
       });
-    });
+    })
   }
 });
 
-let start = (member) => {
-  if (!member || !member.voice.channel) {
-    console.log('no member or vc');
+let start = (member, textChannel, echo) => {
+  if(!member || !member.voice.channel) {
+    console.log('Member does not exist or is not in a voice channel');
     return;
   }
 
   member.voice.channel.join().then((voiceConnection) => {
-    voiceConnections.set(member.voice.channelID, voiceConnection);
-    voiceReceivers.set(member.voice.channelID, voiceConnection.receiver);
+    let channelID = member.voice.channelID;
+    voiceConnections[channelID] = {};
+    voiceConnections[channelID].echo = echo;
+    voiceConnections[channelID].textChannel = textChannel;
+    voiceConnections[channelID].connection = voiceConnection;
+    voiceConnections[channelID].receiver = voiceConnection.receiver;
     if(!echo){
-      saveName = member.voice.channel.name+Date.now()+'.txt';
-      saveStream = fs.createWriteStream(saveName);
+      let saveName = member.voice.channel.name+Date.now()+'.txt';
+      voiceConnections[channelID].saveName = saveName;
+      voiceConnections[channelID].saveStream = fs.createWriteStream(saveName);
     }
   }).catch((error) => {
     console.log("error on channel join");
-  })
+  });
 }
 
 let stop = (member) => {
-
-  if (!member || !member.voice.channel) {
+  if(!member || !member.voice.channel) {
+    console.log('Member does not exist or is not in a voice channel');
     return;
   }
-  if(!echo){
-    console.log(saveName);
-    saveStream.destroy();
+  let channelID = member.voice.channelID;
+  let echo = voiceConnections[channelID].echo;
+  if(!echo) {
+    let saveName = voiceConnections[channelID].saveName;
+    let textChannel = voiceConnections[channelID].textChannel;
+    voiceConnections[channelID].saveStream.destroy();
     textChannel.send({
       files: [{
         attachment: saveName,
@@ -112,16 +121,8 @@ let stop = (member) => {
     }).then(()=>{
       fs.unlinkSync(saveName);
     });
-
-
-
-
   }
-  child.spawn('python', [deletePCM]); //clean up
-  if (voiceReceivers.get(member.voice.channelID)) {
-    voiceReceivers.delete(member.voice.channelID);
-    voiceConnections.get(member.voice.channelID).disconnect();
-    voiceConnections.delete(member.voice.channelID);
-  }
-
+  child.spawn('python', [deletePCM]);
+  voiceConnections[channelID].connection.disconnect();
+  delete voiceConnections[channelID];
 }
